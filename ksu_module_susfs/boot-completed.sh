3208ba781a0022d57aa58b8d42120f04d7dfc8c4
poqdavid <poq-david@live.com>
2026-01-29 16:07:54 +0100

    Squashed 'patches/susfs4ksu/' changes from be8980fba..50e4c35aa
    50e4c35aa KernelSU: Remove duplicated log and changed to a proper log message
    4abb488c0 Refined and renamed hide_sus_mnts_for_all_procs to hide_sus_mnts_for_non_su_procs
    1bbc76fe5 ksu_module_susfs: Updated comments
    211c22fd9 kernel & ksu_module_susfs: Default susfs_hide_sus_mnts_for_all_procs to false to make zygisk more happy
    49361c16d kernel: Fix stat issue when injecting to init.rc for latest Android Canary and qpr3 beta2
    a4c34e587 kernel & KernelSU: Added newfstatat syscall hook for handling latest Android Canary; Sycned with official KernelSU main repo
    
    git-subtree-dir: patches/susfs4ksu
    git-subtree-split: 50e4c35aa2e03e67ad2f045f8fc4ee2098c0b60c

--- ksu_module_susfs/boot-completed.sh
+++ ksu_module_susfs/boot-completed.sh
@@ -68,3 +68,10 @@ ksu_susfs add_sus_map /data/adb/modules/my_module/zygisk/arm64-v8a.so
 ksu_susfs add_sus_map /system/fonts/Roboto-Regular.ttf
 ksu_susfs add_sus_map /system/fonts/RobotoStatic-Regular.ttf
 EOF
+
+#### Unhide all sus mounts from /proc/self/[mounts|mountinfo|mountstat] for non-su processes ####
+## It is suggested to unhide it in this stage, and let kernel or zygisk to umount them for user processes, but this is up to you ##
+cat <<EOF >/dev/null
+ksu_susfs hide_sus_mnts_for_non_su_procs 0
+EOF
+
